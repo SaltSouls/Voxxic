da56dd41fdf26d190dddcc81224cf931ad6383fc
TwinSouls <BeyondTwinSouls@gmail.com>
2021-08-10 22:55:49 -0700

    initial commit

---
+++ src/main/kotlin/com/tainted/voxxic/Voxxic.kt
@@ -0,0 +1,9 @@
+package com.tainted.voxxic
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object Voxxic: ModInitializer {
+    private const val MOD_ID = "voxxic"
+    override fun onInitialize() {
+        println("Voxxic has been initialized.")
+    }
+}
