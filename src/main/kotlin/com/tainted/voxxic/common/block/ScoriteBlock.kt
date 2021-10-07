0d2a9715ea67473516138e87ba51fcf044854293
SaltSouls <BeyondTwinSouls@gmail.com>
2021-10-07 13:05:01 -0700

    Dependencies/general updates

---
+++ src/main/kotlin/com/tainted/voxxic/common/block/ScoriteBlock.kt
@@ -0,0 +1,23 @@
+package com.tainted.voxxic.common.block
+
+import net.minecraft.block.BlockState
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+class ScoriteBlock(settings: Settings): AbstractBurnerBlock(settings) {
+
+    override fun onSteppedOn(world: World, pos: BlockPos, state: BlockState, entity: Entity) {
+        if (isSoaked(state) && !entity.isFireImmune && (entity is LivingEntity) && !EnchantmentHelper.hasFrostWalker(entity)) {
+            entity.damage(DamageSource.HOT_FLOOR, 1.0f)
+        }
+        super.onSteppedOn(world, pos, state, entity)
+    }
+    fun isSoaked(state: BlockState): Boolean {
+        return state.get(AbstractScoriteBlock.SOAKED) as Boolean
+    }
+
+}
